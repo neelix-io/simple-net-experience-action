63c72e8d4fe0be05c57b9de9c4802acaa0cc358c
Nate Plumb <nate.plumb@gmail.com>
2023-08-03 18:51:53 +0800

    generate commentary with markdown table to record keyword occurrences

--- .github/actions/process-reviews/src/index.ts
+++ .github/actions/process-reviews/src/index.ts
@@ -1,6 +1,52 @@
 import * as core from '@actions/core';
 
 
+type ReviewData = {
+  body: string;
+}
+
+type KeywordResults = {
+  [keyword: string]: {
+    count: number;
+    weight: number;
+  };
+}
+
+
+const processKeywords = (keywords: [string, number][], reviews: ReviewData[]) => {
+  const results: KeywordResults = {};
+  for (const [keyword, weight] of keywords) {
+    let count = 0;
+    for (const review of reviews) {
+      const re = new RegExp(keyword, 'g');
+      count += ((<string>review.body)?.match(re)?.length || 0);
+    }
+    results[keyword] = { count, weight }
+  }
+  return results;
+}
+
+const generateCommentary = (results: KeywordResults) => {
+  // columns: keyword, weight, count, total impact
+  const cols = ['keyword', 'weight', 'count', 'total impact'];
+
+  let heading = cols.reduce((acc, col) => `${acc}| ${col.padEnd(10)} `, '');
+  heading += '|\n';
+  cols.forEach(() => {
+    heading += `|-${'-'.repeat(10)}-`;
+  });
+  heading += '|\n';
+
+  return Object.entries(results).reduce((acc, [keyword, { weight, count }]) => {
+    let t = acc;
+    t += `| ${keyword.padEnd(10)} `
+    t += `| ${weight.toString().padEnd(10)} `
+    t += `| ${count.toString().padEnd(10)} `
+    t += `| ${(weight * count).toString().padEnd(10)} |\n`
+    return t;
+  }, heading);
+}
+
 const run = () => {
   const reviewData = core.getInput('review-data');
   console.log('raw review data:', reviewData);
@@ -14,16 +60,17 @@ const run = () => {
     .map(([v, wString]) => [v, +wString] as [string, number]);
   console.log('keywords:', keywords);
 
-  let weight = 0;
-  for (const review of parsedReviews) {
-    for (const [v, w] of keywords) {
-      const re = new RegExp(v, 'g');
-      weight += ((<string>review.body)?.match(re)?.length || 0) * w;
-    }
-  }
+  const results = processKeywords(keywords, parsedReviews);
 
-  core.setOutput('weight', weight);
-  core.setOutput('commentary', 'Commentary output from "process-reviews" action');
+  const aggWeight = Object.values(results)
+    .reduce((acc, { count, weight }) => acc + (count * weight), 0);
+
+  core.setOutput('weight', aggWeight);
+
+  const commentary = generateCommentary(results);
+  console.log('generated commentary:', commentary);
+
+  core.setOutput('commentary', commentary);
 }
 
 run();
